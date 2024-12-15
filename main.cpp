7c23270b7aa32993dc9182c60fe96975b452e87b
Cryptoimba2024 <artenursolov@gmail.com>
2024-12-16 01:23:23 +0200

    last commit

--- main.cpp
+++ main.cpp
@@ -108,3 +108,9 @@ git statusgit status// Пример кода для Matrix Calculator
 // Напишите свой код здесь.
 // Пример кода для Matrix Calculator
 // Напишите свой код здесь.
+// Пример кода для Matrix Calculator
+// Напишите свой код здесь.
+git statusgit add .
+git commit -m "last commit"
+git push
+git push origin main
