577d21dd8694aab8b9a7a6381ff77284aac6de17
Cryptoimba2024 <artenursolov@gmail.com>
2024-12-16 01:18:50 +0200

    last commit

--- main.cpp
+++ main.cpp
@@ -37,5 +37,8 @@
 
 // Пример кода для Matrix Calculator// Пример кода для Matrix Calculator
 // Напишите свой код здесь.
+git status
 
+// Напишите свой код здесь.
+// Пример кода для Matrix Calculator
 // Напишите свой код здесь.
